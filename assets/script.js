7e8a24ffd06509c537fd72dc6f362f1b47dd7cf9
Yosri Jemai <91740252+Yosri-Jemai@users.noreply.github.com>
2025-03-24 02:42:50 +0100

    fix

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,26 @@ const unredNotifications = document.querySelectorAll('.unread');
 const dots = document.querySelectorAll('.dot');
 const count = document.querySelector('.count');
 
-readAll.addEventListener('click', e => {
+function updateCount() {
+    const remainingUnread = document.querySelectorAll('.unread').length;
+    count.innerText = remainingUnread;
+}
+
+readAll.addEventListener('click', () => {
     unredNotifications.forEach(notification => {
         notification.classList.remove('unread');
-    })
+    });
     dots.forEach(dot => {
         dot.classList.remove('dot');
-    })
-    count.innerText = "0";
-})
+    });
+    updateCount();
+});
+
+unredNotifications.forEach(notification => {
+    notification.addEventListener('click', () => {
+        notification.classList.remove('unread');
+        const dot = notification.querySelector('.dot');
+        dot.classList.remove('dot');
+        updateCount(); 
+    });
+});
